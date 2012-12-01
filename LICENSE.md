4efdc490e4896b1f6b4512266a1c3df80b51e74c
Eyal Weiss <weiss.e@gmail.com>
2012-12-01 05:34:41 +0200

    readme file markup

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,5 @@
-=======================================================================================
-WebApp Bootloader
-=======================================================================================
+#WebApp Bootloader
+###Static assets loading and storing in localStorage with cache manifest preloading for offline use and efficient assets versioning.
+
 Copyright 2012, Eyal Weiss
 This may be freely distributed under the MIT license.
-=======================================================================================
--- assets/js/bootloader.js
+++ assets/js/bootloader.js
@@ -1,20 +1,21 @@
 /*!
- * =======================================================================================
+ *
  * WebApp Bootloader
  * =======================================================================================
- * Static assets loading and storing in localStorage
- * with cache manifest preloading for offline use
- * and efficient assets versioning.
- * =======================================================================================
+ *
+ * Static assets loading and storing in localStorage with cache manifest preloading for offline use and efficient assets versioning.
+ * ---------------------------------------------------------------------------------------------------------------------------------
+ *
  * Copyright 2012, Eyal Weiss
  * This may be freely distributed under the MIT license.
- * =======================================================================================
+ * 
  * Tested on WebKit
- * =======================================================================================
+ * 
  * Dependencies:
+ * -------------
  * jQuery
  * Node.js (for building versioned cache manifest and versioned resources index files)
- * =======================================================================================
+ * 
  */
 
 (function () {
